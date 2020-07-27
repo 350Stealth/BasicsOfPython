169bead52d4c529fa267ce17ada6690e12cc1a01
Stealth <350.stealth@gmail.com>
2020-07-27 09:47:06 +0300

    wip week01

--- Week01/Task018/solution.py
+++ Week01/Task018/solution.py
@@ -1,5 +1,5 @@
 num = int(input())
-hours = num // (60 * 60) % 24
-minutes = num // 60 % 60
-seconds = num % 60
-print(hours, ":", minutes // 10, minutes % 10, ":", seconds // 10, seconds % 10, sep="")
+h = num // (60 * 60) % 24
+m = num // 60 % 60
+s = num % 60
+print(h, ":", m // 10, m % 10, ":", s // 10, s % 10, sep="")
